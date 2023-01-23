edbe81991b03bd9912e39eaa8578f274a7ae93f8
Tahseen Dar <tahseendar01@gmail.com>
2023-01-24 00:02:05 +0500

    added files

--- videosLength.js
+++ videosLength.js
@@ -55,22 +55,27 @@ async function processFolders(folderPath, startIndex) {
     const digit = `${contents[i][0]}${
       isNaN(Number(contents[i][1])) ? "" : contents[i][1]
     }`;
+
     // Check if the item is a folder
+    try {
+      if (+digit >= startIndex) {
+        if (fs.statSync(subfolderPath).isDirectory()) {
+          // Process the subfolder and add the total length to the total
+          folderVideoLength = await processFolder(
+            subfolderPath,
+            0,
+            Number.MAX_SAFE_INTEGER
+          );
+          totalLength += folderVideoLength;
 
-    if (+digit >= startIndex) {
-      if (fs.statSync(subfolderPath).isDirectory()) {
-        // Process the subfolder and add the total length to the total
-        folderVideoLength = await processFolder(
-          subfolderPath,
-          0,
-          Number.MAX_SAFE_INTEGER
-        );
-        totalLength += folderVideoLength;
-
-        console.log(contents[i], folderVideoLength / 3600);
+          console.log(contents[i], folderVideoLength / 3600);
+        }
+      } else {
+        totalLength += 0;
       }
-    } else {
-      totalLength += 0;
+    } catch (err) {
+      console.error(err);
+      break;
     }
   }
   return totalLength;
@@ -79,18 +84,30 @@ async function processFolders(folderPath, startIndex) {
 async function main() {
   // Set the path of the folder to process
   const folderPath =
-    "C:\\Users\\dell\\Desktop\\Work\\Courses\\Master the Coding Interview Data Structures + Algorithms\\[TutsNode.com] - Master the Coding Interview Data Structures + Algorithms";
+    "C:\\Users\\dell\\Desktop\\[TutsNode.com] - Complete Machine Learning & Data Science Bootcamp 2021";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\Complete Machine Learning & Data Science Bootcamp 2021\\[TutsNode.com] - Complete Machine Learning & Data Science Bootcamp 2021";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\[TutsNode.com] - JavaScript The Advanced Concepts";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\Complete React Native in 2021 Zero to Mastery with Hooks\\[TutsNode.com] - Complete React Native in 2021 Zero to Mastery with Hooks";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\The Complete Junior to Senior Web Developer Roadmap";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\[FreeCourseSite.com] Udemy - SQL and PostgreSQL The Complete Developer's Guide";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\Infrastructure as Code, Master AWS Cloud Development Kit CDK";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\[TutsNode.com] - TensorFlow Developer Certificate in 2021 Zero to Mastery";
+  // "D:\\Restaurant excel\\Torrent\\Tutorials\\[FreeCourseSite.com] Udemy - SQL and PostgreSQL The Complete Developer's Guide";
+  // "C:\\Users\\dell\\Desktop\\Work\\Courses\\[TutsNode.com] - Complete Python Developer in 2021 Zero to Mastery";
+  // "C:\\Users\\dell\\Desktop\\Work\\Courses\\Master the Coding Interview Data Structures + Algorithms\\[TutsNode.com] - Master the Coding Interview Data Structures + Algorithms";
   // Set the start index for the subfolders
-  const startIndex = 9;
-
-  // Set the end index for the subfolders
-
-  // Get the total length of the videos in the folder and its subfolders
+  const startIndex = 0;
   const totalLength = await processFolders(folderPath, startIndex);
-
-  // Print the total length of the videos in the folder and its subfolders
-  console.log(`Total lengthof videos: ${totalLength / 3600} hours
+  const watchEachDay = 5;
+  const totalLengthInHours = totalLength / 3600;
+  console.log(`Total lengthof videos: ${totalLengthInHours} hours
   `);
+
+  console.log(
+    `If video watched for ${watchEachDay} hours everyday, it will take ${
+      totalLengthInHours / watchEachDay
+    } days to finish this course`
+  );
 }
 
 main();
